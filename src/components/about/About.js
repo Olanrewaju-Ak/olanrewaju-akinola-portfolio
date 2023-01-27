e60ad08d3418b21babd2ac8469f3832cddc9ba2a
Akinola Olanrewaju <103976465+Olanrewaju-Ak@users.noreply.github.com>
2023-01-27 13:10:03 +0100

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,13 +7,14 @@ const About = () => {
 		<section className="about" id="about">
 			<div className="about__container">
 				<div className="about__header">
-					<h1 className="about__header--text">About Me</h1>
+					<h3 className="about__header--text">About Me</h3>
 				</div>
 				<div className="about__content">
 					<div className="about__right-side">
 						<p className="paragraph">
-							Hello, my name is olanrewaju /'O' as in or - 'lan'- 're' as in
-							(ra)y-w-'a' as in America-ju /. I am a full-stack developer with a
+							Hello, my name is Olanrewaju{" "}
+							<span className="transcript">/o-lan-raey-wah-ju/</span>. I am a
+							<span className="highlight"> Full-stack Developer</span> with a
 							background in architecture and design. I love designing and creating
 							things. My passion for human centered designs and using technology to
 							provide creative solutions, led me into web development and I'm keen on
@@ -21,10 +22,10 @@ const About = () => {
 						</p>
 						<br />
 						<p className="paragraph">
-							In my free time, you can catch me watching movies , listening to music ,
-							or following sports. I aslo love arts, hiking and taking photographs
-							(albeit just casually). It is always lovely to enjoy nature with
-							friends.
+							In my free time, you can catch me watching movies, listening to music
+							and podcasts, or following sports. I aslo love arts, hiking and taking
+							photographs (albeit just casually). It is always lovely to enjoy nature
+							with friends.
 						</p>
 					</div>
 					<div className="about__profile-image"></div>
