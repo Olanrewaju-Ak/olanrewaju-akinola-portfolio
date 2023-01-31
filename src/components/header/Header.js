7d6f619f9b4c47685380272c7c0b6c26aeb66589
Olanrewaju-Ak <akinola.lanrewaju@yahoo.com>
2023-01-30 18:22:23 -0800

    rsume link changed to reflect new website domain name

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -50,7 +50,7 @@ const Header = () => {
 					<li className="navbar__list-item" onClick={isMobile ? handleClick : undefined}>
 						<div />
 						<a
-							href="https://drive.google.com/file/d/1JeVGiNzlj1ca6JwkHnWGJYQwPK71Br13/view?usp=share_link"
+							href="https://drive.google.com/file/d/13jgQSkT7D_sACBvMNi0O4kcKfnXkZmFK/view?usp=share_link"
 							target="_blank"
 							rel="noreferrer"
 							className={
