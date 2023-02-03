8f077cc7c040d226616c1eefa21fbc097b457bde
Olanrewaju-Ak <akinola.lanrewaju@yahoo.com>
2023-02-03 10:26:24 -0800

    website revision to clean up the UI

--- src/components/particles-background/ParticlesBackground.js
+++ src/components/particles-background/ParticlesBackground.js
@@ -85,7 +85,7 @@ const ParticlesBackground = () => {
 						type: "circle"
 					},
 					size: {
-						value: { min: 1, max: 3 }
+						value: { min: 1, max: 2 }
 					}
 				},
 				detectRetina: true
